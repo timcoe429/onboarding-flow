c55cd7b91d247025dc40dd24434657a9a279ebd6
timcoe429 <timcoe429@gmail.com>
2025-06-25 11:22:49 -0400

    Update docket-onboarding.php

--- docket-onboarding/docket-onboarding.php
+++ docket-onboarding/docket-onboarding.php
@@ -43,12 +43,6 @@ class DocketOnboarding {
         
         // Register shortcode
         add_shortcode('docket_onboarding', array($this, 'shortcode_handler'));
-        
-        // Add action to enqueue form-specific CSS when forms are loaded via AJAX
-        add_action('wp_ajax_docket_load_fast_build_form', array($this, 'enqueue_fast_build_css'), 5);
-        add_action('wp_ajax_nopriv_docket_load_fast_build_form', array($this, 'enqueue_fast_build_css'), 5);
-        add_action('wp_ajax_docket_load_standard_build_form', array($this, 'enqueue_standard_build_css'), 5);
-        add_action('wp_ajax_nopriv_docket_load_standard_build_form', array($this, 'enqueue_standard_build_css'), 5);
     }
     
     /**
@@ -70,84 +64,31 @@ class DocketOnboarding {
      * Enqueue CSS and JavaScript files
      */
     public function enqueue_scripts() {
-        // Check if we're on a page with the shortcode or if forms are being loaded
-        $load_assets = has_shortcode(get_post_field('post_content', get_the_ID()), 'docket_onboarding') || 
-                       (isset($_REQUEST['action']) && in_array($_REQUEST['action'], array('docket_load_fast_build_form', 'docket_load_standard_build_form')));
+        // Temporarily load on all pages to test
+        // Later we can add back the shortcode check
         
-        if ($load_assets) {
-            // Enqueue main CSS
-            wp_enqueue_style(
-                'docket-onboarding-css',
-                DOCKET_ONBOARDING_PLUGIN_URL . 'assets/onboarding.css',
-                array(),
-                DOCKET_ONBOARDING_VERSION
-            );
-            
-            // Enqueue JavaScript
-            wp_enqueue_script(
-                'docket-onboarding-js',
-                DOCKET_ONBOARDING_PLUGIN_URL . 'assets/onboarding.js',
-                array('jquery'),
-                DOCKET_ONBOARDING_VERSION,
-                true
-            );
-            
-            // Localize script for AJAX
-            wp_localize_script('docket-onboarding-js', 'docket_ajax', array(
-                'ajax_url' => admin_url('admin-ajax.php'),
-                'nonce' => wp_create_nonce('docket_onboarding_nonce')
-            ));
-        }
-    }
-    
-    /**
-     * Enqueue Fast Build Form CSS
-     */
-    public function enqueue_fast_build_css() {
-        // Register and enqueue the fast build form CSS
-        wp_register_style(
-            'docket-fast-build-form-css',
-            DOCKET_ONBOARDING_PLUGIN_URL . 'assets/fast-build-form.css',
+        // Enqueue CSS
+        wp_enqueue_style(
+            'docket-onboarding-css',
+            DOCKET_ONBOARDING_PLUGIN_URL . 'assets/onboarding.css',
             array(),
             DOCKET_ONBOARDING_VERSION
         );
         
-        // Add inline style tag to the form response
-        add_filter('docket_fast_build_form_response', array($this, 'add_fast_build_css_inline'));
-    }
-    
-    /**
-     * Enqueue Standard Build Form CSS
-     */
-    public function enqueue_standard_build_css() {
-        // Register and enqueue the standard build form CSS
-        wp_register_style(
-            'docket-standard-build-form-css',
-            DOCKET_ONBOARDING_PLUGIN_URL . 'assets/standard-build-form.css',
-            array(),
-            DOCKET_ONBOARDING_VERSION
+        // Enqueue JavaScript
+        wp_enqueue_script(
+            'docket-onboarding-js',
+            DOCKET_ONBOARDING_PLUGIN_URL . 'assets/onboarding.js',
+            array('jquery'),
+            DOCKET_ONBOARDING_VERSION,
+            true
         );
         
-        // Add inline style tag to the form response
-        add_filter('docket_standard_build_form_response', array($this, 'add_standard_build_css_inline'));
-    }
-    
-    /**
-     * Add Fast Build CSS inline
-     */
-    public function add_fast_build_css_inline($form_html) {
-        $css_url = DOCKET_ONBOARDING_PLUGIN_URL . 'assets/fast-build-form.css?ver=' . DOCKET_ONBOARDING_VERSION;
-        $css_tag = '<link rel="stylesheet" href="' . esc_url($css_url) . '" type="text/css" media="all" />';
-        return $css_tag . $form_html;
-    }
-    
-    /**
-     * Add Standard Build CSS inline
-     */
-    public function add_standard_build_css_inline($form_html) {
-        $css_url = DOCKET_ONBOARDING_PLUGIN_URL . 'assets/standard-build-form.css?ver=' . DOCKET_ONBOARDING_VERSION;
-        $css_tag = '<link rel="stylesheet" href="' . esc_url($css_url) . '" type="text/css" media="all" />';
-        return $css_tag . $form_html;
+        // Localize script for AJAX (if needed later)
+        wp_localize_script('docket-onboarding-js', 'docket_ajax', array(
+            'ajax_url' => admin_url('admin-ajax.php'),
+            'nonce' => wp_create_nonce('docket_onboarding_nonce')
+        ));
     }
     
     /**

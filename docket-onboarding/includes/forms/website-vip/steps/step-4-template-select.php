cb7312c4bec739a0b4fe9f816b037ac1eb0876d6
timcoe429 <timcoe429@gmail.com>
2025-06-28 18:46:07 -0400

    Update step-4-template-select.php

--- docket-onboarding/includes/forms/website-vip/steps/step-4-template-select.php
+++ docket-onboarding/includes/forms/website-vip/steps/step-4-template-select.php
@@ -1,1 +1,56 @@
+<!-- Step 4: Select Template -->
+<div class="form-step" data-step="4">
+    <h2>Select Your Website Template</h2>
+    <p class="step-subtitle">Click on the images to preview each template</p>
+    
+    <div class="template-grid-2x2">
+        <div class="template-option">
+            <a href="https://dockethosting3.com/salesdemo1" target="_blank" class="template-preview-link">
+                <img src="https://yourdocketonline.com/wp-content/uploads/2025/05/Template-2.png" alt="Template 1">
+                <span class="template-preview-text">Click to View Template 1</span>
+            </a>
+            <label class="template-select">
+                <input type="radio" name="website_template_selection" value="template1" required>
+                <span>Select Template 1</span>
+            </label>
+        </div>
+        
+        <div class="template-option">
+            <a href="https://dockethosting3.com/salesdemo2/" target="_blank" class="template-preview-link">
+                <img src="https://yourdocketonline.com/wp-content/uploads/2025/05/Template-1.png" alt="Template 2">
+                <span class="template-preview-text">Click to View Template 2</span>
+            </a>
+            <label class="template-select">
+                <input type="radio" name="website_template_selection" value="template2" required>
+                <span>Select Template 2</span>
+            </label>
+        </div>
+        
+        <div class="template-option">
+            <a href="https://dockethosting3.com/salesdemo3" target="_blank" class="template-preview-link">
+                <img src="https://yourdocketonline.com/wp-content/uploads/2025/05/Template-3.png" alt="Template 3">
+                <span class="template-preview-text">Click to View Template 3</span>
+            </a>
+            <label class="template-select">
+                <input type="radio" name="website_template_selection" value="template3" required>
+                <span>Select Template 3</span>
+            </label>
+        </div>
+        
+        <div class="template-option">
+            <a href="https://dockethosting3.com/salesdemo4" target="_blank" class="template-preview-link">
+                <img src="https://yourdocketonline.com/wp-content/uploads/2025/05/Template-4.png" alt="Template 4">
+                <span class="template-preview-text">Click to View Template 4</span>
+            </a>
+            <label class="template-select">
+                <input type="radio" name="website_template_selection" value="template4" required>
+                <span>Select Template 4</span>
+            </label>
+        </div>
+    </div>
 
+    <div class="form-nav">
+        <button type="button" class="btn-prev">Back</button>
+        <button type="button" class="btn-next">Next</button>
+    </div>
+</div>

251fdb89fbb7090df5001ee6445007cde02fbafa
timcoe429 <timcoe429@gmail.com>
2025-12-16 09:36:45 -0500

    Update trello-sync.php

--- docket-onboarding/includes/trello-sync.php
+++ docket-onboarding/includes/trello-sync.php
@@ -13,7 +13,7 @@ class DocketTrelloSync {
     
     // Map Trello list names to project status
     private $status_mapping = array(
-        '1. Docket Team' => 'docket_team',
+        '1. Dreamcoders Team' => 'docket_team',
         '2. QA' => 'qa',
         '3. New Builds Ready to Send' => 'ready_to_send',
         '4. Waiting on Review Scheduling' => 'waiting_review_scheduling',
@@ -446,23 +446,23 @@ class DocketTrelloSync {
         
         file_put_contents($trello_debug_log, "[$timestamp] Found " . count($lists) . " lists\n", FILE_APPEND);
         
-        // Find the first list (1. Docket Team)
+        // Find the first list (1. Dreamcoders Team)
         $first_list = null;
-        file_put_contents($trello_debug_log, "[$timestamp] Looking for '1. Docket Team' list...\n", FILE_APPEND);
+        file_put_contents($trello_debug_log, "[$timestamp] Looking for '1. Dreamcoders Team' list...\n", FILE_APPEND);
         foreach ($lists as $list) {
             file_put_contents($trello_debug_log, "[$timestamp] Found list: " . $list['name'] . "\n", FILE_APPEND);
-            if ($list['name'] === '1. Docket Team') {
+            if ($list['name'] === '1. Dreamcoders Team') {
                 $first_list = $list;
                 break;
             }
         }
         
         if (!$first_list) {
-            file_put_contents($trello_debug_log, "[$timestamp] ERROR: '1. Docket Team' list not found\n", FILE_APPEND);
+            file_put_contents($trello_debug_log, "[$timestamp] ERROR: '1. Dreamcoders Team' list not found\n", FILE_APPEND);
             return false;
         }
         
-        file_put_contents($trello_debug_log, "[$timestamp] Found '1. Docket Team' list with ID: " . $first_list['id'] . "\n", FILE_APPEND);
+        file_put_contents($trello_debug_log, "[$timestamp] Found '1. Dreamcoders Team' list with ID: " . $first_list['id'] . "\n", FILE_APPEND);
         
         // Build comprehensive card description
         $card_name = $project_data['business_name'] . ' - ' . ucwords(str_replace('_', ' ', $project_data['form_type']));
